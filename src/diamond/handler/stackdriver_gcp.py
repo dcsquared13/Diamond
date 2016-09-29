22887f77c87c13a1fd8c77c9ee05f14d6581b22c
Dan Cochran <dcochran@nestlabs.com>
2016-09-29 16:31:41 -0700

    adding more functionality to the stackdriver gcp handler.  DEVOPS-24481.

--- src/diamond/handler/stackdriver_gcp.py
+++ src/diamond/handler/stackdriver_gcp.py
@@ -51,6 +51,9 @@ except ImportError:
 class StackdriverGCPHandler(Handler):
 
     def __init__(self, config=None):
+        """
+        Initializations for the Handler.
+        """
         # Initialize Handler
         Handler.__init__(self, config)
         logging.debug("[StackdriverGCPHandler] Initializing Stackdriver GCP Handler")
@@ -85,9 +88,14 @@ class StackdriverGCPHandler(Handler):
                 self.blacklist = self.config['blacklist']
 
         # Initialize Queue
-        self.q = Queue.Queue(int(self.config['max_queue_size']))
-        self.max_queue_time = int(self.config['max_queue_time'])
+        self.q = Queue.PriorityQueue(int(self.config['max_queue_size']))
         self.last_q_push = int(time.time())
+        self.queue_buffer_time = self.config['queue_buffer_time']
+
+        # Statistics
+        self.statistics = self.config['statistics']
+        self.stats_start_time = time.time()
+        self.stats_count = 0
 
     def get_default_config_help(self):
         """
@@ -100,9 +108,10 @@ class StackdriverGCPHandler(Handler):
             'instance_type': 'Instance type',
             'batch_size': 'Number of metrics in batch',
             'max_queue_size': 'Max number of metrics to hold in queue',
-            'max_queue_time': 'Max time a metric should live in queue',
+            'queue_buffer_time': 'Amount of buffer time in queue',
             'tags': 'Tags',
             'blacklist': 'Blacklist of collectors to not process',
+            'statistics': 'Get stats',
         })
 
         return config
@@ -118,9 +127,10 @@ class StackdriverGCPHandler(Handler):
             'instance_type': 'gce_instance',
             'batch_size': 1,
             'max_queue_size': 10000,
-            'max_queue_time': 30,
+            'queue_buffer_time': 120,
             'tags': None,
             'blacklist': None,
+            'statistics': False,
         })
 
         return config
@@ -150,7 +160,6 @@ class StackdriverGCPHandler(Handler):
         Creates a Stackdriver timeseries out of a metric object.
         :param metric: Metric object
         """
-
         path = '%s/%s' % (metric.getCollectorPath().replace(".", "/"),
                           metric.getMetricPath().replace(".", "/"))
         metric_time = self._format_rfc3339(
@@ -199,32 +208,46 @@ class StackdriverGCPHandler(Handler):
         """
         Sending the timeseries to Stackdriver
         :param ts: Timeseries
-        :return:
         """
         logging.debug("[StackdriverGCPHandler] Queue size beginning send: {}"
                       .format(self.q.qsize()))
         metrics = []
+        ts_metrics = []
+        for i in range(num):
+            metrics.append(self.q.get_nowait())
         try:
-            for i in range(num):
-                metrics.append(self.q.get_nowait())
-            body = {'timeSeries': metrics}
+            for i in range(len(metrics)):
+                ts_metrics.append(metrics[i][1])
+            body = {'timeSeries': ts_metrics}
             client = self._get_client()
             request = client.projects().timeSeries().create(
                 name=self.project_resource, body=body)
             request.execute()
             logging.info("[StackdriverGCPHandler] {} custom metrics sent successfully"
                          .format(num))
+            logging.debug("[StackdriverGCPHandler] timeseries sent: {}"
+                          .format(body))
         except Exception, e:
+            # put back on queue.
+            for i in range(len(metrics)):
+                self.q.put_nowait(metrics[i])
             logging.error("[StackdriverGCPHandler] Error sending metrics: {}"
                           .format(e))
         finally:
             # clear list
             del metrics[:]
+            del ts_metrics[:]
             logging.info("[StackdriverGCPHandler] Resetting last push time to {}"
                          .format(int(time.time())))
             self.last_q_push = int(time.time())  # reset last time tried to send
 
     def _check_metric(self, metric):
+        """
+        Check metric to ensure the collector providing it is not blacklisted from
+        sending.
+        :param metric: Metric to check
+        :return:  True or False depending on if blacklisted.
+        """
         ret_code = True
         if self.blacklist:
             logging.debug("[StackdriverGCPHandler] Blacklist {} found processing metrics"
@@ -237,32 +260,62 @@ class StackdriverGCPHandler(Handler):
 
         return ret_code
 
-    def _add_to_queue(self, msg):
+    def _flush_expired_metrics(self):
+        """
+        Flush expired metrics from the queue.
+        """
+        cutoff_time = time.time() - self.queue_buffer_time
+        flush = True
+        while flush:
+            metric = self.q.get_nowait()
+            metric_ts = metric[0]
+            if metric_ts > cutoff_time:
+                # first metric is good, put back on queue and end flush
+                # otherwise drop metric and pull next.
+                self.q.put_nowait(metric)
+                flush = False
+                logging.debug("[StackdriverGCPHandler] Nothing to flush.")
+            else:
+                # Flush this #2 away
+                logging.debug("[StackdriverGCPHandler] flushing {}."
+                              .format(metric))
+
+    def _add_to_queue(self, ts, msg):
         """
         Adds metric to queue.
         :param msg: metric to add to queue
         """
         try:
             logging.debug("[StackdriverGCPHandler] Adding metric to queue.")
-            self.q.put_nowait(msg)
+            tup = (ts, msg)
+            self.q.put_nowait(tup)
+            if self.statistics:
+                if time.time() >= self.stats_start_time + 60:
+                    logging.info("[StackdriverGCPHandler][Statistics] "
+                                  "{} metrics in last {} secs."
+                                  .format(self.stats_count,
+                                          (time.time() - self.stats_start_time)))
+                    self.stats_start_time = time.time()
+                    self.stats_count = 0
+                self.stats_count += 1
         except Queue.Full:
             logging.error("[StackdriverGCPHandler] Queue Full...please investigate!")
         except Exception, e:
             logging.error("[StackdriverGCPHandler] Exception: {}".format(e))
 
     def _process_queue(self):
-        logging.debug("[StackdriverGCPHandler] qsize {}, batch_size {}"
-                      .format(self.q.qsize(), self.batch_size))
+        """
+        Process the queue instigating a send of a batch of metrics to Stackdriver
+        when necessary conditions met.
+        """
+        self._flush_expired_metrics()
         if self.q.qsize() >= self.batch_size:
-            logging.debug("[StackdriverGCPHandler] Batch size met, sending {} metrics"
+            logging.debug("[StackdriverGCPHandler] Sending {} metrics to Stackdriver."
                           .format(self.batch_size))
             self._send(self.batch_size)
-        elif int(time.time()) >= self.last_q_push + self.max_queue_time:
-            logging.debug("[StackdriverGCPHandler] Max queue time reached, sending {} metrics"
-                          .format(self.q.qsize()))
-            self._send(self.q.qsize())
         else:
-            logging.debug("[StackdriverGCPHandler] Nothing to do in process_queue at this time.")
+            logging.debug("[StackdriverGCPHandler] qsize {}, batch_size {}"
+                          .format(self.q.qsize(), self.batch_size))
 
     def process(self, metric):
         """
@@ -272,7 +325,7 @@ class StackdriverGCPHandler(Handler):
         logging.debug("[StackdriverGCPHandler] Metric received: {}".format(metric))
         if self._check_metric(metric):
             timeseries = self._create_timeseries(metric)
-            self._add_to_queue(timeseries)
+            self._add_to_queue(metric.timestamp, timeseries)
             self._process_queue()
 
     def _close(self):
